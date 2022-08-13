751e222b5da55717308f9f38cf1bb6ff524cd404
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-12 23:20:41 -0300

    api implementation

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config()
-const app = express()
+
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -14,10 +18,14 @@ mongoose
     console.log(err)
   })
 
-app.use(express.json())
 
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!")
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -19,9 +19,19 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next()
     } else {
+      res.status(403).json('You are not allowed to do that.')
+    }
+  })
+}
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next()
+    } else {
       res.status(403).json('You are not an Admin.')
     }
   })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin }
